cbf8af16a7c3dee2074e44338511de9c5471a0e3
riko123riko123 <56554387+riko123riko123@users.noreply.github.com>
2023-04-27 14:21:20 +0300

    d

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -73,7 +73,7 @@
                 class="mt-[15px] focus:outline-none text-white bg-[#86efac] hover:bg-[#51d180] focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 0"
                 @click="uploadImage"
               >
-                Upload Image
+                Добавить фото
               </button>
               <p v-if="textImg" class="text-[#4ade80]">Нажмите Save</p>
             </div>
